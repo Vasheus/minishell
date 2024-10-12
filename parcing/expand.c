1aec786813970badbd723be7c0c6e5f08c1edcd0
vasheus <yosabir@e3r1p7.1337.ma>
2024-10-12 16:04:06 +0100

    by vasheus

--- parcing/expand.c
+++ parcing/expand.c
@@ -6,25 +6,22 @@
 /*   By: yosabir <yosabir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:29:10 by yosabir           #+#    #+#             */
-/*   Updated: 2024/10/12 15:29:19 by yosabir          ###   ########.fr       */
+/*   Updated: 2024/10/12 15:58:29 by yosabir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parcing.h"
 
-
-char *expand_var(env_struct *env, const char *var_name) 
+char *get_var_value(char *str)
 {
-    char **str;
-    int i;
+    char *var_name;
+    char *value;
     
-    str = env->env;
-    i = 0;
-    while(str[i] != NULL)
-    {
-        if (ft_strcmp(str[i], var_name) == 0 && str[i][ft_strlen(var_name)] == '=') 
-            return (str[i] + ft_strlen(var_name) + 1);
-        i++;
-    }
-    return NULL;
+    var_name = str + 1;
+    value = getenv(var_name);
+    
+    if(value)
+        return(value);
+    else
+        return("");
 }
--- parcing/minishell.c
+++ parcing/minishell.c
@@ -6,7 +6,7 @@
 /*   By: yosabir <yosabir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 11:10:03 by yosabir           #+#    #+#             */
-/*   Updated: 2024/10/12 15:30:38 by yosabir          ###   ########.fr       */
+/*   Updated: 2024/10/12 16:01:32 by yosabir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ int main(int ac, char **av)
     (void)ac;
     (void)av;
     
-    char        *input;
-    t_list      *lst;
-    t_list      *tmp;
+    char *input;
+    t_list *lst;
+    t_list *tmp;
+    set_args *arg;
 
     while (1)
     {
@@ -34,13 +35,40 @@ int main(int ac, char **av)
         syntax(&lst);
         classing(&lst);
         
-        settingargs(&lst);
+        arg = settingargs(&lst);
 
         tmp = lst;
+        
+        
+        //PRINTING TOKENS AND TYPE OF COMMAND
         while (tmp)
         {
             printf("Token: %s, Type: %u\n", (char *)tmp->content, tmp->command);
             tmp = tmp->next;
+        }
+        
+        
+        printf("#########\n#########\n#########\n#########\n");
+        
+        
+        set_args *current_args = arg;
+        
+        // PRINTING THE ARGUMENTS AND THE INPUT/OUTPUT FD
+        while (current_args)
+        {
+            printf("Input: %d, Output: %d\n", current_args->input, current_args->output);
+            if (current_args->args)
+            {
+                printf("Arguments: ");
+                int i = 0;
+                while (current_args->args[i] != NULL)
+                {
+                    printf("%s ", current_args->args[i]);
+                    i++;
+                }
+                printf("\n");
+            }
+            current_args = current_args->next;
         }
 
         free(input);
