535ab18a75f9e0fbdd3c0a6485e6df64ebb23911
vasheus <yosabir@e3r1p7.1337.ma>
2024-10-12 16:54:38 +0100

    by vasheus

--- parcing/ofred.c
+++ parcing/ofred.c
@@ -6,7 +6,7 @@
 /*   By: yosabir <yosabir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:11:35 by yosabir           #+#    #+#             */
-/*   Updated: 2024/10/12 15:58:22 by yosabir          ###   ########.fr       */
+/*   Updated: 2024/10/12 16:50:43 by yosabir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,72 +27,50 @@ int pipe_counter(t_list **lst)
     }
     return (i + 1);
 }
-void handle_redirections(t_list *token, set_args *cmd_args)
+int open_file(const char *filename, int flags) 
+{
+    int fd;
+    
+    fd = open(filename, flags);
+    if (fd == -1)
+        perror("Error opening file");
+    return (fd);
+}
+
+t_list *skip_spaces(t_list *token) 
+{
+    while (token && token->command == SPACE)
+        token = token->next;
+    return token;
+}
+
+void handle_redirections(t_list *token, set_args *cmd_args) 
 {
     cmd_args->input = 0;
     cmd_args->output = 1;
-    if(token->command == RD_OUT)
+
+    if (token->command == RD_OUT) 
     {
-        if(cmd_args->output != 1)
+        if (cmd_args->output != 1)
             close(cmd_args->output);
-        cmd_args->output = open(token->next->content, O_CREAT | O_WRONLY , 644);
+        token = skip_spaces(token->next);
+        if (token)
+            cmd_args->output = open_file(token->content, O_CREAT | O_RDWR | 644);
     }
-    else if(token->command == RD_IN)
-        cmd_args->input = open(token->next->content, O_RDONLY);
-    else if(token->command == APPEND)
+    else if (token->command == RD_IN) 
     {
-        if(cmd_args->output != 1)
+        token = skip_spaces(token->next);
+        if (token)
+            cmd_args->input = open_file(token->content, O_RDONLY);
+    }
+    else if (token->command == APPEND) 
+    {
+        if (cmd_args->output != 1)
             close(cmd_args->output);
-        cmd_args->output = open(token->next->content, O_CREAT | O_WRONLY | O_APPEND, 644);
+        token = skip_spaces(token->next);
+        if (token)
+            cmd_args->output = open_file(token->content, O_CREAT | O_WRONLY | O_APPEND | 644);
     }
     else if (token->command == HEREDOC)
         cmd_args->output = create_unique_heredoc_file();
-    
 }
-void add_arg_to_set_args(set_args *cmd_args, char *new_arg)
-{
-    char **new_args;
-    int count;
-    int i;
-
-    count = 0;
-    i = 0;
-    while (cmd_args->args && cmd_args->args[count])
-        count++;
-    new_args = (char **)malloc(sizeof(char *) * (count + 2));
-    if (!new_args)
-        return;
-
-    while (i < count)
-    {
-        new_args[i] = cmd_args->args[i];
-        i++;
-    }
-    new_args[count] = ft_strdup(new_arg);
-    new_args[count + 1] = NULL;
-
-    free(cmd_args->args);
-    cmd_args->args = new_args;
-}
-
-void storing_args(t_list **current, set_args *cmd_args)
-{
-    while (*current && (*current)->command != PIPE)
-    {
-        if ((*current)->command == WORD || (*current)->command == S_QUOTE ||
-            (*current)->command == D_QUOTE)
-            add_arg_to_set_args(cmd_args, (*current)->content);
-        else if ((*current)->command == VAR)
-        {
-            char *var_value;
-            
-            var_value = get_var_value((*current)->content);
-            add_arg_to_set_args(cmd_args, var_value);
-        }
-        else if ((*current)->command == RD_IN || (*current)->command == RD_OUT ||
-                 (*current)->command == APPEND || (*current)->command == HEREDOC)
-            handle_redirections(*current, cmd_args);
-        *current = (*current)->next;
-    }
-}
-
--- parcing/settinglist.c
+++ parcing/settinglist.c
@@ -6,12 +6,59 @@
 /*   By: yosabir <yosabir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 16:44:50 by yosabir           #+#    #+#             */
-/*   Updated: 2024/10/10 16:47:16 by yosabir          ###   ########.fr       */
+/*   Updated: 2024/10/12 16:49:06 by yosabir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parcing.h"
 
+void add_arg_to_set_args(set_args *cmd_args, char *new_arg)
+{
+    char **new_args;
+    int count;
+    int i;
+
+    count = 0;
+    i = 0;
+    while (cmd_args->args && cmd_args->args[count])
+        count++;
+    new_args = (char **)malloc(sizeof(char *) * (count + 2));
+    if (!new_args)
+        return;
+
+    while (i < count)
+    {
+        new_args[i] = cmd_args->args[i];
+        i++;
+    }
+    new_args[count] = ft_strdup(new_arg);
+    new_args[count + 1] = NULL;
+
+    free(cmd_args->args);
+    cmd_args->args = new_args;
+}
+
+void storing_args(t_list **current, set_args *cmd_args)
+{
+    while (*current && (*current)->command != PIPE)
+    {
+    
+        if ((*current)->command == WORD || (*current)->command == S_QUOTE ||
+            (*current)->command == D_QUOTE)
+            add_arg_to_set_args(cmd_args, (*current)->content);
+        else if ((*current)->command == VAR)
+        {
+            char *var_value;
+            
+            var_value = get_var_value((*current)->content);
+            add_arg_to_set_args(cmd_args, var_value);
+        }
+        else if ((*current)->command == RD_IN || (*current)->command == RD_OUT ||
+                 (*current)->command == APPEND || (*current)->command == HEREDOC)
+            handle_redirections(*current, cmd_args);
+        *current = (*current)->next;
+    }
+}
 static set_args *init_set_args(void)
 {
     set_args *cmd_args;
